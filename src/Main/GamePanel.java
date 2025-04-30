ddc74f742962549e87564bdc5e822da25372997c
Nguyen Duc Hai <ngduchaidev0301@gmail.com>
2025-04-30 09:18:05 +0700

    add gamethread and run method

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int orginalTilesizes = 16;
     final int scale = 3;
     final int tileSize = orginalTilesizes * scale;
@@ -12,10 +12,20 @@ public class GamePanel extends JPanel {
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
     
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+    @Override
+    public void run(){
+
+    }
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 }
