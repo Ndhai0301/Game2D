6664077824f2913f90c57c3a34a7602722207fa4
Nguyen Duc Hai <ngduchaidev0301@gmail.com>
2025-04-30 10:20:22 +0700

    add key handler

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -2,6 +2,8 @@ package Main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -13,19 +15,48 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
     
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
     @Override
     public void run(){
-
+        while (gameThread != null){
+          System.out.println("The gameloop is running");  
+          update();
+          repaint();
+        }
     }
     public void startGameThread(){
         gameThread = new Thread(this);
         gameThread.start();
+    }
+    public void update(){
+        if (keyH.upPressed == true){
+            playerY -= playerSpeed;
+        } else if (keyH.downpressed == true){
+            playerY += playerSpeed;
+        }   else if(keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }   else if (keyH.leftPressed == true){
+            playerY -= playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
     }
 }
---
+++ src/Main/KeyHandler.java
@@ -0,0 +1,40 @@
+package Main;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class KeyHandler implements KeyListener {
+    public boolean upPressed, downpressed, leftPressed, rightPressed;
+    public void keyTyped(KeyEvent e){
+    }
+    public void keyPressed(KeyEvent e){
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W){
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S){
+            downpressed = true;
+        }
+        if (code == KeyEvent.VK_A){
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_V){
+            rightPressed = true;
+        }
+    }
+
+    public void keyReleased(KeyEvent e){
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W){
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_S){
+            downpressed = false;
+        }
+        if (code == KeyEvent.VK_A){
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_V){
+            rightPressed = false;
+        }
+    }
+    
+}
